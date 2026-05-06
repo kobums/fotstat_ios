98040f3a198a318339f24e426b3183f47dbd9b06
고범석 <kobums@naver.com>
2026-05-06 19:39:42 +0900

    Initial Commit

---
+++ fotstat/fotstatApp.swift
@@ -0,0 +1,17 @@
+//
+//  fotstatApp.swift
+//  fotstat
+//
+//  Created by gowoobro on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct fotstatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
